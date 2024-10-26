e065dc5f921bacaa0f7c372437c1c3a7d51b7f38
jb <jbdubillard@hotmail.com>
2024-10-26 21:33:40 +0200

    Projet initialization

---
+++ MovieFinder/MovieFinder/MovieFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFinderApp.swift
+//  MovieFinder
+//
+//  Created by Jean-baptiste DUBILLARD on 26/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
